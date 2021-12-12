7715b590f20b2942fe8753c8fc530e00961eeb40
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2021-12-11 23:31:42 -0300

    Ux upgrading on login page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,6 +8,19 @@ import firebaseFirestoreService from '../../services/firebase/firebase-firestore
 
 import styles from './home.module.scss'
 
+import LogoOpenMind from './images/Logo-openmind.png'
+import LogoOpus from './images/Logo-opus.png'
+import Shape1 from './images/shape-1.png'
+import Shape2 from './images/shape-2.png'
+import Shape3 from './images/shape-3.png'
+import Shape4 from './images/shape-4.png'
+import Shape5 from './images/shape-5.png'
+import Shape6 from './images/shape-6.png'
+import Shape7 from './images/shape-7.png'
+import Shape8 from './images/shape-8.png'
+import Shape81 from './images/shape-81.png'
+import Shape9 from './images/shape-9.png'
+
 const HomePage = () => {
 
     const [cpfData, setCpfData] = useState("")
@@ -38,7 +51,21 @@ const HomePage = () => {
     return (
         <div className={styles.container}>
             <div className={styles.containerLeft}>
-                <h1>Bingopus.</h1>
+                <img id={styles.openMind} src={LogoOpenMind} alt="logo-openmind" />
+                <img id={styles.opusLogo} src={LogoOpus} alt="logo opus" />
+                <img id={styles.shape1} src={Shape1} alt="forma abstrata" />
+                <img id={styles.shape2} src={Shape2} alt="forma abstrata" />
+                <img id={styles.shape3} src={Shape3} alt="forma abstrata" />
+                <img id={styles.shape4} src={Shape4} alt="forma abstrata" />
+                <img id={styles.shape5} src={Shape5} alt="forma abstrata" />
+                <img id={styles.shape6} src={Shape6} alt="forma abstrata" />
+                <img id={styles.shape7} src={Shape7} alt="forma abstrata" />
+                <div className={styles.shapeGroup}>
+                    <img id={styles.shape8} src={Shape8} alt="forma abstrata" />
+                    <img id={styles.shape81} src={Shape81} alt="forma abstrata" />
+                    <img id={styles.shape82} src={Shape8} alt="forma abstrata" />
+                </div>
+                <img id={styles.shape9} src={Shape9} alt="forma abstrata" />
             </div>
 
             <Form
