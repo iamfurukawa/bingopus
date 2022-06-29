1fccc62e9b60ee0db495f46836c722afe04493d7
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2022-06-29 09:40:14 -0300

    Festa JuLina:  - Limpar gabarito!

--- src/components/board-write/index.js
+++ src/components/board-write/index.js
@@ -6,6 +6,7 @@ import SquareComponent from '../square'
 import FirebaseRealtimeService from '../../services/firebase/firebase-realtime-service'
 
 import styles from './board.module.scss'
+import { Button } from 'antd'
 
 var md5 = require('md5')
 
@@ -29,6 +30,11 @@ const BoardComponent = () => {
     FirebaseRealtimeService.updateBoard(board)
   }
 
+  const clearBoard = () => {
+    setBoard(new Array(75).fill(0))
+    FirebaseRealtimeService.updateBoard(new Array(75).fill(0))
+  }
+
   const readFromRealtimeFirebase = () => {
     monitoringRef.on('value', async (snap) => {
       try {
@@ -50,30 +56,35 @@ const BoardComponent = () => {
   }
 
   return (
-    <div className={styles.card}>
-      <div className={styles.container}>
+    <>
+      <div className={styles.card}>
+        <div className={styles.container}>
 
-        <div className={styles.letters}>B</div>
-        <Column init={1} end={15} />
-        <div className={styles.letters}>B</div>
+          <div className={styles.letters}>B</div>
+          <Column init={1} end={15} />
+          <div className={styles.letters}>B</div>
 
-        <div className={styles.letters}>I</div>
-        <Column init={16} end={30} />
-        <div className={styles.letters}>I</div>
+          <div className={styles.letters}>I</div>
+          <Column init={16} end={30} />
+          <div className={styles.letters}>I</div>
 
-        <div className={styles.letters}>N</div>
-        <Column init={31} end={45} />
-        <div className={styles.letters}>N</div>
+          <div className={styles.letters}>N</div>
+          <Column init={31} end={45} />
+          <div className={styles.letters}>N</div>
 
-        <div className={styles.letters}>G</div>
-        <Column init={46} end={60} />
-        <div className={styles.letters}>G</div>
+          <div className={styles.letters}>G</div>
+          <Column init={46} end={60} />
+          <div className={styles.letters}>G</div>
 
-        <div className={styles.letters}>O</div>
-        <Column init={61} end={75} />
-        <div className={styles.letters}>O</div>
+          <div className={styles.letters}>O</div>
+          <Column init={61} end={75} />
+          <div className={styles.letters}>O</div>
+        </div>
       </div>
-    </div>
+      <Button type="danger" onClick={clearBoard}>
+        Limpar Gabarito
+      </Button>
+    </>
   )
 }
 
