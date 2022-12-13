8376cf21c6d701d9d3ea9ce81b6832084c155639
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2022-12-13 17:30:21 -0300

    Festa final de ano:  - Bugfix button disabled  - New Images  - FIx letter

--- src/components/board-write/modal/index.jsx
+++ src/components/board-write/modal/index.jsx
@@ -7,6 +7,8 @@ import { Button } from 'antd'
 import bingo1 from './bingo1.gif'
 import bingo2 from './bingo2.gif'
 import bingo3 from './bingo3.gif'
+import bingo4 from './bingo4.gif'
+import bingo5 from './bingo5.gif'
 
 import styles from './modal.module.scss'
 
@@ -17,23 +19,38 @@ const CageGiveAway = ({ numbers, closeModal, updateBoard }) => {
     const [image, setImage] = useState()
 
     useEffect(() => {
-        setImage(_.shuffle([bingo1, bingo2, bingo3])[0])
+        setImage(_.shuffle([bingo1, bingo2, bingo3, bingo4, bingo5])[0])
+
         const pairNumbers = numbers.map((isSelected, index) => ({ index, isSelected }))
         const notSelected = pairNumbers.filter(pair => !pair.isSelected)
         setShuffled(_.shuffle(notSelected)[0])
     }, [])
 
     const showNumberAndSave = () => {
+        if (show) return
         updateBoard(shuffled.index)
         setShow(true)
+    }
+
+    const letter = (number) => {
+        if (number <= 15)
+            return 'B'
+        if (number <= 30)
+            return 'I'
+        if (number <= 45)
+            return 'N'
+        if (number <= 60)
+            return 'G'
+        if (number <= 75)
+            return 'O'
     }
 
     return (
         <div className={styles.content}>
             <img src={image} alt="bingo!" />
-            {show ? <h1>{shuffled.index + 1}</h1> : <h1>??</h1>}
+            {show ? <h1>{letter(shuffled.index + 1) + '' + (shuffled.index + 1)}</h1> : <h1>??</h1>}
             <div className={styles.buttons}>
-                <Button type="danger" onClick={showNumberAndSave}>
+                <Button type="danger" onClick={showNumberAndSave} disabled={show}>
                     Mostrar Número
                 </Button>
                 <Button type="danger" onClick={() => closeModal()}>
